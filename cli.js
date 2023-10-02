61fa967682ad0f595fe3e1f1f5748733857f578c
Dustin Thurston <dthurston1996@gmail.com>
2023-10-02 11:46:34 -0400

    Halloween theme for the month of October

--- cli.js
+++ cli.js
@@ -2,22 +2,47 @@
 const figlet = require("figlet");
 const data = require("./lib/data");
 const { render } = require("prettyjson");
+const colors = require('colors');
 
-const renderOptions = {
-    dashColor: "cyan",
-    keysColor: "magenta",
-    stringColor: "white"
+
+function createBannerString(font) {
+    return figlet.textSync(
+        "DustyCode!",
+        {
+            font: font,
+            horizontalLayout: "universal smushing",
+            verticalLayout: "default",
+            width: 110,
+            whitespaceBreak: true
+        }
+    )
 }
 
-console.log(figlet.textSync(
-    "DustyCode!",
-    {
-        font: "Chunky",
-        horizontalLayout: "universal smushing",
-        verticalLayout: "default",
-        width: 110,
-        whitespaceBreak: true
+let renderOptions;
+let font;
+let banner;
+
+const today = new Date()
+
+if(today.getMonth() == 9) {
+    font = "Poison"
+    renderOptions = {
+        dashColor: "red",
+        keysColor: "yellow",
+        stringColor: "white"
     }
-));
+    data["message"] = "Happy Halloween!"
+    banner = colors.red(createBannerString(font));
+} else {
+    font = "Chunky"
+    renderOptions = {
+        dashColor: "cyan",
+        keysColor: "magenta",
+        stringColor: "white"
+    }
+    banner = colors.magenta(createBannerString(font));
+}
+
+console.log(banner);
 
 console.log(render(data, renderOptions))
